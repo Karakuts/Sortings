39e7a209efad97ee3395761e5c854d1c1e5301d3
Дмитрий Каракуц <nokva513@gmail.com>
2021-04-15 13:05:42 +0300

    fix

--- Sortings/Program.cs
+++ Sortings/Program.cs
@@ -9,13 +9,13 @@ namespace SortingsProject
   {
     static void Main(string[] args)
     {
-      int[] someArray = { 6, 8, 4, 7, 11, 5, 10, 9, 1, 2, 3 };
+      int[] someArray = { 6, 8, 4, 7, 11, 5, 10, 9, 1, 2, 3, 12 };
 
       BubbleSorting bubbleSorting = new BubbleSorting(new ConsolePrinterGraphSorting(), new SwapSorting());
       bubbleSorting.BeginSorting(someArray);
 
       Console.ReadLine();
-      someArray = new[] { 6, 8, 4, 7, 11, 5, 10, 9, 1, 2, 3 };
+      someArray = new[] { 6, 8, 4, 7, 11, 5, 10, 9, 1, 2, 3, 12 };
 
       ShakerSorting shakerSorting = new ShakerSorting(new ConsolePrinterGraphSorting(), new SwapSorting());
       shakerSorting.BeginSorting(someArray);
